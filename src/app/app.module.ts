ce29f5c212db5ba7746dd14c7bff2fdaae78ed1e
sriram.j <sriram.j@gofrugal.com>
2018-02-19 13:44:58 +0530

    material angular configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
+/**Common Angular Imports */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+/**Commin Angular imports end */
 
+/**Material Angular Imports */
+import { MatButtonModule } from '@angular/material/button'
+/**Material Angular Imports End*/
+
+/**Importing Modules */
 import { AppRoutingModule } from './app-routing.module';
+/**Importing Modules End */
 
+/**Importing Components */
 import { AppComponent } from './app.component';
+/**Importing Components End*/
+
+/**Importing Services */
+/**Importing Services End*/
 
 
 @NgModule({
@@ -12,7 +26,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
